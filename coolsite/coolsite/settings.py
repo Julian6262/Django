3186c0698fd6fbc70fe19de59cb08c17cb015e28
Julian6262 <d.baburin62@gmail.com>
2023-07-11 01:40:59 +0300

    init

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-yi1fyz7kh6ee%-sbn1y&7t4^5o30w57)uy!c=s+r*16il@j(61
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -111,6 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,10 +1,31 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.shortcuts import redirect, render
+from .models import *
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 
 def index(request):
-    return HttpResponse("Страница приложения women.")
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request, catid):
     return HttpResponse(f"<h1>Статьи по категориям</h1>{catid}</p>")
+
+
+def archive(request, year):
+    # if (int(year) > 2020):
+    #     raise Http404()
+    if (int(year) > 2020):
+        return redirect('home', permanent=True)
+
+    return HttpResponse(f"<h1>Архив по годам</h1>{year}</p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена11</h1>')
